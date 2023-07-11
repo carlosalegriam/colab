0ae4b5263b5b969776517e087dc4ca1d83d5c514
Carlos Alegria Moena <calegriam@protonmail.com>
2023-07-11 01:30:29 +0000

    Course y Name

--- colabapp/academy/views.py
+++ colabapp/academy/views.py
@@ -5,3 +5,7 @@ def v_index(request):
     context = {}
     return render(request, 'index.html', context)
 
+def v_course(request):
+    context = {}
+    return render(request, 'course.html', context)
+
