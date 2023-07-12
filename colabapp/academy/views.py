995b9c28a5851e3c112c0124c1a406f04f2b3212
Carlos Alegria Moena <calegriam@protonmail.com>
2023-07-12 01:50:40 +0000

    Urls de Cursos creados

--- colabapp/academy/views.py
+++ colabapp/academy/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
 def v_index(request):
-    context = {}
+    context = {
+        'coursel': Course.objects.get(name = 'Matematica Avanzada'),
+        'course2': Course.objects.get(name = 'Lógica de programación'),
+    }
     return render(request, 'index.html', context)
 
 def v_course(request):
-    context = {}
+    context = {
+        'course': Course.objects.get(id = course_id)
+    }
     return render(request, 'course.html', context)
 
